eb019b56de4f8d1075c803f8a1556a5c6bd91f53
Khai-code <quangkhai2004bv@gmail.com>
2024-09-26 08:48:25 +0700

    data

---
+++ DATN_Teacher/API/Program.cs
@@ -0,0 +1,36 @@
+using Data.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+var m = "m";
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy(name: m, policy => policy.AllowAnyOrigin()
+                                              .AllowAnyMethod()
+                                              .AllowAnyHeader());
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
